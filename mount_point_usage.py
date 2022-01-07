19aa9e03f2bddacc93fb8aee3d9899394be6da4d
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-01-07 14:13:10 +0200

    list namespaces

--- mount_point_usage.py
+++ mount_point_usage.py
@@ -1,1 +1,7 @@
-print("hello world")
+import subprocess
+import sys
+##Get a list of the namespaces. 
+##subprocess.check_output is returing bytes , so we conver bytes to string with decode and string to list with split
+list_of_namespaces = subprocess.check_output("kubectl get ns --no-headers -o jsonpath='{.items[*].metadata.name}'",shell=True).decode(sys.stdout.encoding).split(" ")
+#print(type(list_of_namespaces))
+#print(list_of_namespaces)
