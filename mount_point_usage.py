dcf8edbd3b557e09d559b44efd195c87d1ca7c84
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-01-07 14:35:06 +0200

    get pods per namespace

--- mount_point_usage.py
+++ mount_point_usage.py
@@ -5,3 +5,10 @@ import sys
 list_of_namespaces = subprocess.check_output("kubectl get ns --no-headers -o jsonpath='{.items[*].metadata.name}'",shell=True).decode(sys.stdout.encoding).split(" ")
 #print(type(list_of_namespaces))
 #print(list_of_namespaces)
+##Get a list of pods per namespace
+##Use the same convertion like namespaces to convert from bytes to a list
+for current_namespace in list_of_namespaces:
+ ##print(current_namespace)
+ list_of_pods_per_namespace = subprocess.check_output("kubectl -n %s  get po -o jsonpath='{.items[*].metadata.name}'" % current_namespace,shell=True).decode(sys.stdout.encoding).split(" ")
+ print(type(list_of_pods_per_namespace))
+ print(list_of_pods_per_namespace)
