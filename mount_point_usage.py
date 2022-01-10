4a25c9593ba05f4bad22f5114af6026c883c264a
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-01-10 13:55:36 +0200

    runcommand function

--- mount_point_usage.py
+++ mount_point_usage.py
@@ -9,6 +9,7 @@ if code!=0:
   print("-----------------------------------------------------------------")
   print(f"For the command: {get_namespaces_command} \nThe error is : {err}")
   print("-----------------------------------------------------------------")
+  exit()
 ##Convert str output to list
 list_of_namespaces = stdout.split(" ")
 ##debug##print(type(list_of_namespaces))
@@ -17,23 +18,30 @@ list_of_namespaces = stdout.split(" ")
 ##Get a list of pods per namespace
 ##Use the same convertion like namespaces to convert from str to a list
 for current_namespace in list_of_namespaces:
-  ##print(current_namespace)
-  get_po_per_namespace_command"kubectl -n %s  get po -o jsonpath='{.items[*].metadata.name}'" % current_namespace
-  code1 stdout2 err2 = runcommand(get_po_per_namespace_command)
+  ##debug##print(current_namespace)
+  get_po_per_namespace_command="kubectl -n %s  get po -o jsonpath='{.items[*].metadata.name}'"%current_namespace
+  ##debug##print(get_po_per_namespace_command)
+  code2, stdout2, err2 = runcommand(get_po_per_namespace_command)
+  if code2!=0:
+    print("-----------------------------------------------------------------")
+    print(f"For the command: {get_po_per_namespace_command} \nThe error is : {err2}")
+    print("-----------------------------------------------------------------")
+    exit()
   list_of_pods_per_namespace = stdout2.split(" ")
- #debug#print(type(list_of_pods_per_namespace))
- print(list_of_pods_per_namespace)
- if len(list_of_pods_per_namespace) > 0 and len(list_of_pods_per_namespace[0]) >0 :
-  for pod in list_of_pods_per_namespace:
-   returned_code = os.system("kubectl -n {0} exec -it {1} -- mount 2>/dev/null|grep {2}".format(current_namespace,pod,nfsserver))
-   #debug#print(current_namespace,pod)
-   if returned_code == 0:
-    command_mount = 'kubectl -n {0} exec -it {1} -- mount 2>/dev/null|grep {2}'
-    mounts_per_pod = subprocess.check_output(command_mount.format(current_namespace,pod,nfsserver),shell=True).decode(sys.stdout.encoding).split(" ")
+  ##debug##print(type(list_of_pods_per_namespace))
+  ##debug##print(list_of_pods_per_namespace)
+  if len(list_of_pods_per_namespace) > 0 and len(list_of_pods_per_namespace[0]) >0 :
+   for pod in list_of_pods_per_namespace:
+     list_mounts_command = "kubectl -n %s exec -it %s -- mount 2>/dev/null|grep %s"%(current_namespace, pod, nfsserver)
+     code3, stdout3, err3 = runcommand(list_mounts_command)
+     print(stdout3)
+     if code3 == 0:
+      command_mount = 'kubectl -n {0} exec -it {1} -- mount 2>/dev/null|grep {2}'
+      mounts_per_pod = subprocess.check_output(command_mount.format(current_namespace,pod,nfsserver),shell=True).decode(sys.stdout.encoding).split(" ")
     
-    substring = "var/lib/kubelet/pods"
-    if not substring in mounts_per_pod[2]:
-     command_df = 'kubectl -n {0} exec -it {1} -- df -hP {2}'
-     df_usage= subprocess.check_output(command_df.format(current_namespace,pod,mounts_per_pod[2]),shell=True).decode(sys.stdout.encoding).split(" ")
-     if int(df_usage[-2].replace("%", "")) >= usage_threshold:
-      print("You need to check usage for mount point ", df_usage[-1]," .The usage is ", df_usage[-2],"The pod is ",pod," and the namespace is ",current_namespace)
+      substring = "var/lib/kubelet/pods"
+      if not substring in mounts_per_pod[2]:
+       command_df = 'kubectl -n {0} exec -it {1} -- df -hP {2}'
+       df_usage= subprocess.check_output(command_df.format(current_namespace,pod,mounts_per_pod[2]),shell=True).decode(sys.stdout.encoding).split(" ")
+       if int(df_usage[-2].replace("%", "")) >= usage_threshold:
+        print("You need to check usage for mount point ", df_usage[-1]," .The usage is ", df_usage[-2],"The pod is ",pod," and the namespace is ",current_namespace)
