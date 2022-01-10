97ce3315da1c510cef95414d89c8270c51ecf69f
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-01-10 14:20:01 +0200

    runcommand function

--- mount_point_usage.py
+++ mount_point_usage.py
@@ -15,9 +15,10 @@ list_of_namespaces = stdout.split(" ")
 ##debug##print(type(list_of_namespaces))
 ##debug##print(list_of_namespaces)
 
-##Get a list of pods per namespace
-##Use the same convertion like namespaces to convert from str to a list
+##For all the namespaces
 for current_namespace in list_of_namespaces:
+  ##Get a list of pods per namespace
+  ##Use the same convertion like namespaces to convert from str to a list
   ##debug##print(current_namespace)
   get_po_per_namespace_command="kubectl -n %s  get po -o jsonpath='{.items[*].metadata.name}'"%current_namespace
   ##debug##print(get_po_per_namespace_command)
@@ -30,7 +31,18 @@ for current_namespace in list_of_namespaces:
   list_of_pods_per_namespace = stdout2.split(" ")
   ##debug##print(type(list_of_pods_per_namespace))
   ##debug##print(list_of_pods_per_namespace)
-  if len(list_of_pods_per_namespace) > 0 and len(list_of_pods_per_namespace[0]) >0 :
+  get_pvc_per_namespace_command="kubectl -n %s  get pvc -o jsonpath='{.items[*].metadata.name}'"%current_namespace
+  ##debug##print(get_pvc_per_namespace_command)
+  code4, stdout4, err4 = runcommand(get_pvc_per_namespace_command)
+  if code2!=0:
+    print("-----------------------------------------------------------------")
+    print(f"For the command: {get_po_per_namespace_command} \nThe error is : {err4}")
+    print("-----------------------------------------------------------------")
+    exit()
+  list_of_pvcs_per_namespace = stdout4.split(" ")
+  ##If the namespace has at least one pod and there is at least one pvc at the namespace
+  print(current_namespace,list_of_pods_per_namespace,list_of_pvcs_per_namespace)
+  if len(list_of_pods_per_namespace) > 0 and len(list_of_pods_per_namespace[0]) >0 and len(list_of_pvcs_per_namespace) >0 and len(list_of_pods_per_namespace[0]) >0 :
    for pod in list_of_pods_per_namespace:
      list_mounts_command = "kubectl -n %s exec -it %s -- mount 2>/dev/null|grep %s"%(current_namespace, pod, nfsserver)
      code3, stdout3, err3 = runcommand(list_mounts_command)
