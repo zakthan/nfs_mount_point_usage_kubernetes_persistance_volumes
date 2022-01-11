6d70221cf141140f0b45c728174b1e4202c06784
Zakopoulos Athanasios <azakopoulos@ote.gr>
2022-01-11 10:46:22 +0200

    rm

--- test.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-##subprocess.check_output('k exec -it bb -- mount|grep aaa')
-aaa= subprocess.check_output(["kubectl", "get", "po"])
-print(aaa)
-subprocess.check_call(['ls'], stdout = DEVNULL, stderr = STDOUT)
